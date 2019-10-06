dc1df028fd90be3b4d1bc0f23652a57e4a6f9201
Илья Долженко <ilay00@mail.ru>
2019-10-06 15:11:39 +0300

    Updates

---
+++ precache-manifest.2eacee7ae12dece1ed553ce014e83e1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "583f6a18ca337b9bbc9f9d65c41bf17d",
+    "url": "/kinopoisk/index.html"
+  },
+  {
+    "revision": "0ea7526f46bf16d11fcc",
+    "url": "/kinopoisk/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "afa7707902f35b2c8205",
+    "url": "/kinopoisk/static/css/main.c91a2854.chunk.css"
+  },
+  {
+    "revision": "0ea7526f46bf16d11fcc",
+    "url": "/kinopoisk/static/js/2.e20e81b5.chunk.js"
+  },
+  {
+    "revision": "afa7707902f35b2c8205",
+    "url": "/kinopoisk/static/js/main.6607fa16.chunk.js"
+  },
+  {
+    "revision": "94a58e9b7e2cbe82af1c",
+    "url": "/kinopoisk/static/js/runtime-main.6f3abbd2.js"
+  }
+]);
