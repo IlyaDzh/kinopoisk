a628e1691fb146fa6aab75ab37588e7f695de367
Илья Долженко <ilay00@mail.ru>
2019-11-11 15:16:55 +0300

    fix rerender

--- src/components/FilmDetails.js
+++ src/components/FilmDetails.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Loader from 'react-loader-spinner'
 
+import ErrorPage from './ErrorPage';
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 
 class FilmDetails extends React.Component {
@@ -8,20 +9,55 @@ class FilmDetails extends React.Component {
         super(props);
         this.state = {
             details: [],
-            load: false
+            load: false,
+            error: false
         };
     }
 
     componentDidMount() {
         const DETAILS_URL = `https://api.themoviedb.org/3/movie/${this.props.match.params.filmId}?api_key=3ac9e9c4b5b41ada30de1c0b1e488050&language=ru`;
-        fetch(DETAILS_URL).then(value => {
-            return value.json();
+        fetch(DETAILS_URL).then(response => {
+            if (!response.ok) {
+                throw new Error("HTTP status " + response.status);
+            }
+            return response.json();
         }).then(output => {
             this.setState({
                 details: output,
                 load: true
             });
-        })
+        }).catch(error => {
+            this.setState({
+                load: true,
+                error: true
+            });
+        });
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.match.params.filmId !== prevProps.match.params.filmId) {
+            this.setState({
+                error: false,
+                load: false
+            });
+            const DETAILS_URL = `https://api.themoviedb.org/3/movie/${this.props.match.params.filmId}?api_key=3ac9e9c4b5b41ada30de1c0b1e488050&language=ru`;
+            fetch(DETAILS_URL).then(response => {
+                if (!response.ok) {
+                    throw new Error("HTTP status " + response.status);
+                }
+                return response.json();
+            }).then(output => {
+                this.setState({
+                    details: output,
+                    load: true
+                });
+            }).catch(error => {
+                this.setState({
+                    load: true,
+                    error: true
+                });
+            });
+        }
     }
 
     render() {
@@ -30,51 +66,50 @@ class FilmDetails extends React.Component {
                 <div className='content loader'>
                     <Loader type="Oval" color="#444" height={80} width={80} />
                 </div> :
-                <div className='content details'
-                    // style={{
-                    //     backgroundImage: `url(https://image.tmdb.org/t/p/original/${this.state.details.backdrop_path})`,
-                    //     backgroundSize: 'cover',
-                    //     backgroundRepeat: 'no-repeat',
-                    //     backgroundPosition: 'center center'
-                    // }}
-                >
-                    <div className='details-overlay'>
-                        <img className='details-overlay__img' src={`https://image.tmdb.org/t/p/original/${this.state.details.backdrop_path}`} alt={this.state.details.title} />
-                    </div>
-                    <div className='details-wrapper info'>
-                        <div className='info__vote'>
-                            {this.state.details.vote_average}
+                !this.state.error ?
+                    <div className='content details'
+                        style={{
+                            backgroundImage: `url(https://image.tmdb.org/t/p/original/${this.state.details.backdrop_path})`,
+                            backgroundSize: 'cover',
+                            backgroundRepeat: 'no-repeat',
+                            backgroundPosition: 'center center'
+                        }}
+                    >
+                        <div className='details-wrapper info'>
+                            <div className='info__vote'>
+                                {this.state.details.vote_average}
+                            </div>
+                            <div className='info__title'>
+                                {this.state.details.title}
+                            </div>
+                            <div className='info__subtitle'>
+                                {this.state.details.original_title}
+                            </div>
+                            <div className='info__date'>
+                                {this.state.details.release_date.substring(4, 0)}
+                            </div>
+                            <ul className='info__genres'>
+                                {
+                                    this.state.details.genres.map((item, index) => {
+                                        return <li key={item.id}>{(index ? '| ' : '') + item.name}</li>
+                                    })
+                                }
+                            </ul>
+                            <div className='info__time'>
+                                {this.state.details.runtime}
+                            </div>
+                            <div className='info__budget'>
+                                ${this.state.details.budget}
+                            </div>
+                            <div className='info__tagline'>
+                                {this.state.details.tagline}
+                            </div>
+                            <div className='info__overview'>
+                                {this.state.details.overview}
+                            </div>
                         </div>
-                        <div className='info__title'>
-                            {this.state.details.title}
-                        </div>
-                        <div className='info__subtitle'>
-                            {this.state.details.original_title}
-                        </div>
-                        <div className='info__date'>
-                            {this.state.details.release_date.substring(4, 0)}
-                        </div>
-                        <ul className='info__genres'>
-                            {
-                                this.state.details.genres.map((item, index) => {
-                                    return <li key={item.id}>{ (index ? '| ' : '') + item.name}</li>
-                                })
-                            }
-                        </ul>
-                        <div className='info__time'>
-                            {this.state.details.runtime}
-                        </div>
-                        <div className='info__budget'>
-                            ${this.state.details.budget}
-                        </div>
-                        <div className='info__tagline'>
-                            {this.state.details.tagline}
-                        </div>
-                        <div className='info__overview'>
-                            {this.state.details.overview}
-                        </div>
-                    </div>
-                </div>
+                    </div> :
+                    <ErrorPage />
         );
     }
 }
